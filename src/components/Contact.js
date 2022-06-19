b343ea6c1f0c27ea3441b3c2ecade7ea85c74668
mnaauval <mnaauval20@gmail.com>
2022-06-20 00:13:21 +0700

    update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,30 +3,29 @@
 import React from "react";
 import "./contact.css";
 
-const Contact = ({ data }) => {
+const Contact = ({ contacts }) => {
   return (
-    // <div className="container__card">
-    //   {contacts.map((contact) => (
-    //     <div className="card" key={contact.id}>
-    //       <img src={contact.photo} alt={contact.name} />
-    //       <div className="card__details">
-    //         <h2>{contact.name}</h2>
-    //         <p>{contact.phone}</p>
-    //         <p>{contact.email}</p>
-    //       </div>
-    //     </div>
-    //   ))}
-
     <div className="container__card">
-      <div className="card">
-        <img src={data.photo} alt={data.name}></img>
-        <section className="card__details">
-          <h3>{data.name}</h3>
-          <p>{data.phone}</p>
-          <p>{data.email}</p>
-        </section>
-      </div>
+      {contacts.map((contact) => (
+        <div className="card" key={contact.id}>
+          <img src={contact.photo} alt={contact.name} />
+          <div className="card__details">
+            <h2>{contact.name}</h2>
+            <p>{contact.phone}</p>
+            <p>{contact.email}</p>
+          </div>
+        </div>
+      ))}
     </div>
+    // <div className="container__card">
+    //   <div className="card">
+    //     <img src={data.photo} alt={data.name}></img>
+    //     <section className="card__details">
+    //       <h3>{data.name}</h3>
+    //       <p>{data.phone}</p>
+    //       <p>{data.email}</p>
+    //     </section>
+    //   </div>
   );
 };
 
