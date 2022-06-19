1221dbc265425450352ecbffe6c0622988b4622d
mnaauval <mnaauval20@gmail.com>
2022-06-19 23:52:41 +0700

    feat: Header, Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,23 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
+import "../style/contact.css";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = ({ contacts }) => {
+  return (
+    <div className="container__card">
+      {contacts.map((contact) => (
+        <div className="card" key={contact.id}>
+          <img src={contact.photo} alt={contact.name} />
+          <div className="card__details">
+            <h2>{contact.name}</h2>
+            <p>{contact.phone}</p>
+            <p>{contact.email}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Contact;
