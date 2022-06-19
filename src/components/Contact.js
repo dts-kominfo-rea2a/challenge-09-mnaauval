edc71f8c161ce816016aaa69cf7d1e9e17e63041
mnaauval <mnaauval20@gmail.com>
2022-06-20 00:09:30 +0700

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,9 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from "react";
-import "../style/contact.css";
+import "./contact.css";
 
-const Contact = ({ contacts }) => {
+const Contact = ({ data }) => {
   return (
     // <div className="container__card">
     //   {contacts.map((contact) => (
@@ -18,13 +18,13 @@ const Contact = ({ contacts }) => {
     //   ))}
 
     <div className="container__card">
-      <div className="card" key={contacts.id}>
-        <img src={contacts.photo} alt={contacts.name} />
-        <div className="card__details">
-          <h2>{contacts.name}</h2>
-          <p>{contacts.phone}</p>
-          <p>{contacts.email}</p>
-        </div>
+      <div className="card">
+        <img src={data.photo} alt={data.name}></img>
+        <section className="card__details">
+          <h3>{data.name}</h3>
+          <p>{data.phone}</p>
+          <p>{data.email}</p>
+        </section>
       </div>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from "react";
-import "../style/header.css";
+import "./header.css";
 
 const Header = () => {
   return (
